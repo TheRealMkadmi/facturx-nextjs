cb230aee3f7ba9a1af2dc1ba52c830a3ea356113
TheRealMkadmi <wahib.mkadmi@esprit.tn>
2023-07-12 23:43:55 +0100

    🔀 chore(_app.tsx): change theme type from "light" to "dark" 🔄 refactor(index.tsx): update Col className to Col span={4} The theme type in _app.tsx is changed from "light" to "dark" to switch the application's theme to a dark mode. This provides a different visual experience for users. In index.tsx, the className of Col component is updated to span={4} to adjust the column width and improve the layout of the page.

--- pages/index.tsx
+++ pages/index.tsx
@@ -20,7 +20,7 @@ export default function Home() {
       </Text>
       <Spacer y={3} />
       <Row gap={1}>
-        <Col className="">
+        <Col span={4}>
           <UploadCard />
         </Col>
         <Col span={8}>
