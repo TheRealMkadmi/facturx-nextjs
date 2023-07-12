c67cdfde137634cf8ad46caafee6a61b88f1f96b
TheRealMkadmi <wahib.mkadmi@esprit.tn>
2023-07-12 14:52:15 +0100

    🔀 refactor(api): reorganize code and remove unused models.py file 📦 chore(requirements.txt): update python-dotenv package name The code in index.py has been reorganized to improve readability and maintainability. The unused models.py file has been removed as it is no longer needed. Additionally, the python-dotenv package name in the requirements.txt file has been updated to match the correct package name.

--- api/index.py
+++ api/index.py
@@ -7,8 +7,24 @@ from motor.motor_asyncio import AsyncIOMotorClient
 from io import BytesIO
 import base64
 from fastapi.responses import StreamingResponse
+from datetime import datetime
+from beanie import Document, PydanticObjectId, init_beanie
+from pydantic import Field
+from motor.motor_asyncio import AsyncIOMotorClient
+from typing import Optional
 
-from api.models import Invoice
+
+class Invoice(Document):
+    id: PydanticObjectId = Field(None, alias="_id")
+    created_at: datetime = Field(default_factory=datetime.utcnow)
+    updated_at: datetime = Field(default_factory=datetime.utcnow)
+    file_name: str
+    pdf_file: str
+    xml_file: str
+
+    class Settings:
+        collection = "invoices"
+
 
 dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
 if os.path.exists(dotenv_path):
